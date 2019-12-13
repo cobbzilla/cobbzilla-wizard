745b18232ae1db6f82b1252dddcf037f8558e7fd
Jonathan Cobb <jonathan@kyuss.org>
2019-12-13 16:45:54 -0500

    remove obsolete classes

--- wizard-server/src/main/java/org/cobbzilla/wizard/resources/FileUploadContext.java
+++
@@ -1,4 +0,0 @@
-package org.cobbzilla.wizard.resources;
-
-public interface FileUploadContext {
-}
--- wizard-server/src/main/java/org/cobbzilla/wizard/server/config/HasAssetStorageConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.cobbzilla.wizard.server.config;
-
-import org.cobbzilla.wizard.asset.AssetStorageService;
-
-public interface HasAssetStorageConfiguration {
-
-    AssetStorageService getAssetStorageService();
-
-}
