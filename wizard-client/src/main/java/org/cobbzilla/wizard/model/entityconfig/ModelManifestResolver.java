ab5f863cec672b778287c8dc9b661441a85603a7
Jonathan Cobb <jonathan@kyuss.org>
2020-01-05 13:32:59 -0500

    use static import

--- wizard-client/src/main/java/org/cobbzilla/wizard/model/entityconfig/ModelManifestResolver.java
+++ wizard-client/src/main/java/org/cobbzilla/wizard/model/entityconfig/ModelManifestResolver.java
@@ -1,10 +1,9 @@
 package org.cobbzilla.wizard.model.entityconfig;
 
-import org.cobbzilla.util.json.JsonUtil;
-
 import java.util.LinkedHashMap;
 
 import static org.cobbzilla.util.io.FileUtil.dirname;
+import static org.cobbzilla.util.json.JsonUtil.FULL_MAPPER_ALLOW_COMMENTS;
 import static org.cobbzilla.util.json.JsonUtil.json;
 
 public interface ModelManifestResolver {
@@ -14,7 +13,7 @@ public interface ModelManifestResolver {
     String jsonAtPath(String path);
 
     default LinkedHashMap<String, String> buildModel(String path) {
-        final String[] models = json(jsonAtPath(path), String[].class, JsonUtil.FULL_MAPPER_ALLOW_COMMENTS);
+        final String[] models = json(jsonAtPath(path), String[].class, FULL_MAPPER_ALLOW_COMMENTS);
         final LinkedHashMap<String, String> modelJson = new LinkedHashMap<>(models.length);
         final String parent = dirname(path);
         for (String model : models) {
