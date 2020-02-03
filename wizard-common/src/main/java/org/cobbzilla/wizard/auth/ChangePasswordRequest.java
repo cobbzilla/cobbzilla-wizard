1c4d29aef889254e046e781378e35012853d32a6
Jonathan Cobb <jonathan@kyuss.org>
2020-02-03 17:29:05 -0500

    add totpToken to ChangePasswordRequest

--- wizard-common/src/main/java/org/cobbzilla/wizard/auth/ChangePasswordRequest.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/auth/ChangePasswordRequest.java
@@ -6,16 +6,21 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.cobbzilla.wizard.validation.HasValue;
 
+import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
+
 @NoArgsConstructor @Accessors(chain=true)
 public class ChangePasswordRequest {
 
     @Getter @Setter private String uuid;
 
-    @HasValue(message="error.changePassword.oldPassword.required")
+    @HasValue(message="err.currentPassword.required")
     @Getter @Setter private String oldPassword;
 
-    @HasValue(message="error.changePassword.newPassword.required")
+    @HasValue(message="err.password.required")
     @Getter @Setter private String newPassword;
+
+    @Getter @Setter private String totpToken;
+    public boolean hasTotpToken () { return !empty(totpToken); }
 
     @Getter @Setter private boolean sendInvite = false;
 
