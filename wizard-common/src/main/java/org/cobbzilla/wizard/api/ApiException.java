073106e5cd843b8ff303aa1ea0ffce99f8bdf1df
Jonathan Cobb <jonathan@kyuss.org>
2020-07-19 20:39:18 -0400

    add status code to ApiException message

--- wizard-common/src/main/java/org/cobbzilla/wizard/api/ApiException.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/api/ApiException.java
@@ -6,19 +6,17 @@ import lombok.ToString;
 import org.cobbzilla.util.http.HttpRequestBean;
 import org.cobbzilla.wizard.util.RestResponse;
 
-import static org.cobbzilla.util.json.JsonUtil.json;
-
 @AllArgsConstructor @ToString
 public class ApiException extends RuntimeException {
 
     @Getter private HttpRequestBean request;
-    @Getter private RestResponse response;
+    @Getter private final RestResponse response;
 
     public ApiException (RestResponse response) {
         super(response.status+": "+response.json);
         this.response = response;
     }
 
-    @Override public String getMessage () { return response.json; }
+    @Override public String getMessage () { return response.status + ": " + response.json; }
 
 }
